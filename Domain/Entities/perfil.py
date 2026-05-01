6cbe6eb91c30675858915724a4f27e6243f31527
Piero <ironpiero1@gmail.com>
2026-05-01 13:01:41 -0300

    fix bug perfil

--- Domain/Entities/perfil.py
+++ Domain/Entities/perfil.py
@@ -5,7 +5,7 @@ class perfil:
     id_atleta:int
     objetivos:str
 
-    def __init__(sefl, id_perfil:int, id_atleta:int, objetivos:str):
+    def __init__(self, id_perfil:int, id_atleta:int, objetivos:str):
         self.id_perfil=id_perfil
         self.id_atleta=id_atleta
         self.objetivos=objetivos
